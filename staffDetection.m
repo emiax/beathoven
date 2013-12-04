f5089a146beddc7c72a2f2b3e5a00991262da3e7
emiax <mail@emilaxelsson.se>
2013-12-04 14:35:24 +0100

    staff detection

--- staffDetection.m
+++ staffDetection.m
@@ -1,31 +1,46 @@
-function staffs = staffDetection(BWimg)
+% Returns a matrix of n*5 elements, where n is the number of staffs.
+
+function staffs = staffDetection(input)
+    
+    input = bwmorph(input,'erode');
+
+    f = [-1 0 1];
+    derivative = imfilter(input,f,'circular');
+    
     %Projects everything on the horizontal plane
-    horHist = sum(BWimg == 0, 2)/sum(size(BWimg));
+    projectedDerivative = sum(derivative, 2)/sum(size(derivative));
+    projectedInput = sum(input == 0, 2)/sum(size(input));
     
-    %Taken from and modified http://stackoverflow.com/questions/6549591/split-an-array-in-matlab
-    %In order to find seperated parts of our horizontal projection
-    edgeArray = diff([0; (horHist(:) ~= 0); 0]);    
-    indices = [find(edgeArray > 0) find(edgeArray <0)-1];
+    linePoints = max(projectedInput-10*projectedDerivative, 0);
+    [peaks, locations] = findpeaks(linePoints, 'MINPEAKDISTANCE', 2);
     
     
-    threshHist = zeros(size(horHist));
-    for i = 1:size(indices,1)
-        group = horHist(indices(i,1):indices(i,2));
+    
+    %iterate over possible upper lines
+    first = 1;
+    last = length(locations) - 4;
+    weights = zeros([last, 1]);
+    staffs = [];
+    for i = first:last
+        distances = locations(i:i+3) - locations(i+1:i+4);
+        diff = sum(abs(distances(2:4) - distances(1:3)));
         
-        threshold = graythresh(group);
-        group = im2bw(group, threshold);
+        % calculate product of all five line points
+        pointProduct = prod(peaks(i:i+4)); % maybe needed for improvements
         
-        
-        [~, locations] = findpeaks(im2double(group), 'MINPEAKDISTANCE', 3);
-        
-        staffGroup = zeros(size(group));
-        staffGroup(locations) = group(locations);
-        sum(staffGroup == 1)
-        
-        if sum(staffGroup == 1) >= 5
-            threshHist(indices(i,1):indices(i,2)) = staffGroup;
+        % sum of difference cannot be smaller than 10 pixels.
+        % TODO: weight on image size.
+        if (diff < 10)
+            staffs = cat(1, staffs, locations(i:i+4)');
         end
     end
-    staffs = threshHist;
+    
+    %Plot for debugging purposes:
+    %figure; hold on;
+    %imshow(input);
+    %for y = staffs(:)
+    %    plot([0, 1000], [y y], 'r');
+    %end
+   
+    
 end
-
