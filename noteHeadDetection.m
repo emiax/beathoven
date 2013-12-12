29eb0dc280a8344d83347e259e073e59e9b44502
Karl Johan Krantz <schwomp@gmail.com>
2013-12-12 15:27:51 +0100

    TopHeat instead of openning

--- noteHeadDetection.m
+++ noteHeadDetection.m
@@ -6,28 +6,37 @@ function [ noteHeads ] = noteHeadDetection( imgNoLines, lines)
 
     lineDist = floor(mean(mean(lines(:,2:5) - lines(:, 1:4))));
 
+    %Element for closing
+    strClose = strel('ball', round(lineDist/3), 1, 2);
+    strClose = strClose.getnhood.*strClose.getheight;
+    strClose = imresize(strClose, 'Scale', [1,1]);
+    strClose = imrotate(strClose, 45, 'bilinear');
+    
+    %Element for opening
+    strOpen = strel('ball', round(lineDist/3), 1, 2);
+    strOpen = strOpen.getnhood.*strOpen.getheight;
+    strOpen = imresize(strOpen, 'Scale', [1,1.2]);
+    strOpen = imrotate(strOpen, 45, 'bilinear');
 
-    str = strel('ball', round(lineDist/3), 1, 2);
-
-    str = str.getnhood.*str.getheight;
-
-    str = imresize(str, 'Scale', [1,1.2]);
-    str = imrotate(str, 45, 'bilinear');
-
-    %bwImg = thresh(staff);  
-    %morphed = bwmorph(bwImg, 'clean', Inf);
-    %closed = imfill(staff);
-
-    correlation = imopen(imgNoLines, round(str));
-
+    
+    
+    morphed = imgNoLines;
+    
+    
+    morphed = imtophat(morphed, round(strOpen));
+    morphed = imtophat(morphed, round(strOpen));
+    
+    correlation = imgNoLines-morphed;
     
     noteHeads = 1 - (correlation./max(max(double(correlation))));
     
-    %debugImage = zeros(zeros(h,w,3));
-    %debugImage(:,:,3) = noteHeads;
+    %debugImage = zeros(h,w,3);
+    %debugImage(:,:,3) = 1-noteHeads;
     %debugImage(:,:,2) = imgNoLines;
     %figure();
     %imshow(debugImage);
+    
+    
     
 end
 
