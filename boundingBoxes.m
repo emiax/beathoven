b5aaac4d8242f20cf9551522507cf84991e4f319
emiax <mail@emilaxelsson.se>
2013-12-15 16:13:02 +0100

    cleanup

--- boundingBoxes.m
+++ boundingBoxes.m
@@ -1,19 +1,14 @@
-function [boxes, headCentroids, flagPositions] = boundingBoxes(stems, heads, lineDist, staffImg, topLine, bottomLine)
-  both = (stems + heads) > 0;
+function [boxes, headCentroids, flagPositions] = boundingBoxes(stems, heads, lineDist)
+    both = (stems + heads) > 0;
     both = imdilate(both, ones(round(lineDist*1/3)));
 
     [h, w] = size(stems);
 
-    categorization = zeros(h, w, 3);
-    categorization(:, :, 1) = heads;
-    categorization(:, :, 2) = stems;
-    categorization(:, :, 3) = 1-staffImg;
+    %categorization = zeros(h, w, 3);
+    %categorization(:, :, 1) = heads;
+    %categorization(:, :, 2) = stems;
+    %categorization(:, :, 3) = 1-heads-stems;
     
-    
-    %figure();
-    %imshow(categorization);
-    %hold on;
-      
     labels = bwlabel(both, 8);
     boxesStruct = regionprops(labels, 'BoundingBox');
     nBoxes = numel(boxesStruct);
@@ -23,8 +18,6 @@ function [boxes, headCentroids, flagPositions] = boundingBoxes(stems, heads, lin
         boxes(i,1:4) = boxesStruct(i).BoundingBox;
     end
     
-   
-    
     boxes = sortrows(boxes, 1);
   
     headCentroids = cell([nBoxes, 1]);
@@ -32,24 +25,22 @@ function [boxes, headCentroids, flagPositions] = boundingBoxes(stems, heads, lin
     flagPositions = zeros(nBoxes, 3);
     
     [nBoxes, ~] = size(boxes);
-    %warning off;
+
     for i = 1:nBoxes;
 
         bb = round(boxes(i, 1:4));
         
         x = max(bb(1), 1); y = max(bb(2), 1); x2 = min(x+bb(3), w); y2 = min(y+bb(4), h);
-        
-        
+         
         stemXOffset = 0;
         [~, stemXOffset] = max(vertProj(stems(y:y2,x:x2)));
         
         
         localHeads = heads(y:y2,x:x2);
         headLabels = bwlabel(localHeads);
-        headCentroidsStruct = regionprops(headLabels, 'Centroid', 'Area', 'BoundingBox');
+        headCentroidsStruct = regionprops(headLabels, 'Centroid', 'Area');
 
         nHeads = numel(headCentroidsStruct);
-         
         
         foundInTop = [];
         foundInBottom = [];
@@ -58,9 +49,6 @@ function [boxes, headCentroids, flagPositions] = boundingBoxes(stems, heads, lin
            hb = headCentroidsStruct(j).Centroid;
            ha = headCentroidsStruct(j).Area;
            
-           hbb = headCentroidsStruct(j).BoundingBox;
-           
-           %plot(x + [hbb(1) hbb(3)], y + [hbb(2) hbb(4)], 'r');
            
            headX = x+hb(1);
            headY = y+hb(2);
@@ -68,15 +56,12 @@ function [boxes, headCentroids, flagPositions] = boundingBoxes(stems, heads, lin
            %plot(headX, headY, 'gx');
            %plot(headX, topLine, 'rx');
            %plot(headX, bottomLine, 'mx');
-           
            %headX = x+hbb(1)+hbb(3)/2;
            %headY = y+hbb(2)+hbb(4)/2;
            
            headArea = y+ha;
            
            ratio = (headY - y) / (y2 - y);
-           
-           
            
            if (ratio > 3/4)
                foundInBottom(end + 1) = j;
@@ -111,18 +96,10 @@ function [boxes, headCentroids, flagPositions] = boundingBoxes(stems, heads, lin
         if (bottomArea == 0 && topArea == 0)
             headCentroids{i} = [];
         end
-        
-        
-        
-        
     end
-%    warning on;
 
-    
-    
     % uncomment for debugging
     %figure();
-    
     %imshow(categorization); hold on;
     %for i = 1:nBoxes
     %    rectangle('Position', boxes(i, :), 'EdgeColor','r');
@@ -135,9 +112,6 @@ function [boxes, headCentroids, flagPositions] = boundingBoxes(stems, heads, lin
     %        plot(x, y,'g.');
     %    end
     %end
-%         
-%     
-%
 end
 
 
